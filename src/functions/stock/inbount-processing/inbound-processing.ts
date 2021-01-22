ac3f89e0d9fee7b48fb684f33bec0c57c73ba6c9
Pedro Oliveira <pedrodepaivasoaresll@gmail.com>
2021-01-22 00:07:28 -0300

    feat: inbound processing

--- src/functions/stock/inbount-processing/inbound-processing.ts
+++ src/functions/stock/inbount-processing/inbound-processing.ts
@@ -1,7 +1,75 @@
-import { DynamoDB } from 'aws-sdk'
+import { DynamoDBStreamHandler } from 'aws-lambda'
+import { DynamoDB, dynamoClient } from '../../../libs'
+import { transformObjectKeysToCamel } from '../../../helpers'
+import { StockInboundKeys, DynamoDBTables, StockKeys } from '../../../types'
 
-export const stockInboundProcessingHandler = async (
-  event: DynamoDB.GetItemOutput
+export async function getStockBy(id: string) {
+  const TableName: DynamoDBTables = 'stockTable'
+  const params: DynamoDB.GetItemInput = {
+    TableName,
+    Key: {
+      id: {
+        S: id,
+      },
+    },
+  }
+
+  return dynamoClient.getItem(params).promise()
+}
+
+export async function updateStockQuantityBy(id: string, newQuantity: string) {
+  const TableName: DynamoDBTables = 'stockTable'
+  const params: DynamoDB.UpdateItemInput = {
+    TableName,
+    Key: {
+      id: {
+        S: id,
+      },
+    },
+    UpdateExpression: 'set quantity = :q',
+    ExpressionAttributeValues: {
+      ':q': {
+        N: newQuantity,
+      },
+    },
+  }
+
+  return dynamoClient.updateItem(params).promise()
+}
+
+export const stockInboundProcessingHandler: DynamoDBStreamHandler = async (
+  event
 ) => {
-  console.log('caiu na trigger', event)
+  event.Records.forEach(async (value) => {
+    if (!value.dynamodb?.NewImage) {
+      throw new Error('new image is undefined')
+    }
+
+    const inbound = transformObjectKeysToCamel(
+      value.dynamodb?.NewImage
+    ) as Record<StockInboundKeys, DynamoDB.AttributeValue>
+
+    const { stockId, inboundQuantity } = inbound
+
+    if (!stockId.S) {
+      throw new Error('stock id is undefined')
+    }
+
+    const { $response: stockResponse } = await getStockBy(stockId.S)
+    const { data: stock } = stockResponse
+
+    if (!stock || !stock.Item) {
+      throw new Error('stock not found')
+    }
+
+    const stockData = transformObjectKeysToCamel(stock.Item) as Record<
+      StockKeys,
+      DynamoDB.AttributeValue
+    >
+
+    const updatedStockQuantity =
+      Number(stockData.quantity.N) + Number(inboundQuantity.N)
+
+    await updateStockQuantityBy(stockId.S, String(updatedStockQuantity))
+  })
 }
--- src/types/stock.ts
+++ src/types/stock.ts
@@ -1,3 +1,13 @@
+export type StockKeys =
+  | 'id'
+  | 'externalId'
+  | 'quantity'
+  | 'cost'
+  | 'quantityMin'
+  | 'unitSalePrice'
+  | 'expanses'
+  | 'provider'
+
 export interface Stock {
   id: string
   externalId: string
@@ -10,6 +20,13 @@ export interface Stock {
 }
 
 export type StockInboundStatus = 'PENDING' | 'ACCOUNTED'
+
+export type StockInboundKeys =
+  | 'id'
+  | 'stockId'
+  | 'eventAt'
+  | 'inboundQuantity'
+  | 'status'
 
 export interface StockInbound {
   id: string
